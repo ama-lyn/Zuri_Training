f1e886c74f5239eb6e79f84bf879de2dfb9e8c67
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-09-10 21:58:34 +0300

    Update Stage One

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ updateUTCTime();
 
 // Set an interval to update the UTC time
 setInterval(updateUTCTime, 1000);
+
+// Function to get and display the current day
+function getCurrentDay() {
+  // Days of the week
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  // Get the current date
+  const currentDate = new Date();
+
+  const dayIndex = currentDate.getDay();
+
+  const currentDay = daysOfWeek[dayIndex];
+
+  const currentDayElement = document.querySelector(
+    '[data-testid="currentDayOfTheWeek"]'
+  );
+  currentDayElement.textContent = currentDay;
+}
+
+// Call the function to display the current day
+getCurrentDay();
